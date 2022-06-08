27ebfa6aee4e6764eab4b603b97850fab80f3e3e
Catalin Apostu <dev.catalin.apostu@gmail.com>
2022-06-08 23:00:58 +0200

    Configure Next.js project

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  swcMinify: true,
+  images: {
+    domains: ["images.ctfassets.net"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+      {
+        source: "/candidat/:slug*",
+        destination: "https://integritatepebune.ro/politruc/:slug*",
+      },
+    ];
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "accent-1": "#333",
+      },
+      height: {
+        144: "36rem",
+        150: "37.5rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
